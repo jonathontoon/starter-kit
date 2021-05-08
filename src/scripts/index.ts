7a7e7cb44171094f6afd5bc062622b3ab01dc23f
Jonathon Toon <jonathontoon@gmail.com>
2021-05-08 13:24:57 +1000

    ts lint

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,17 +1,17 @@
-(function () {
+(function (): void {
 
-  /*
+	/*
     Private Functions
   */
 
-  function handleOnDOMContentLoaded() {
-    console.log("Loaded");
-  };
+	function handleOnDOMContentLoaded(event: Event): void {
+		console.log(event);
+	};
 
-  /*
+	/*
     Events
   */
 
-  document.addEventListener("DOMContentLoaded", handleOnDOMContentLoaded, false);
+	document.addEventListener("DOMContentLoaded", handleOnDOMContentLoaded, false);
 
 })();
